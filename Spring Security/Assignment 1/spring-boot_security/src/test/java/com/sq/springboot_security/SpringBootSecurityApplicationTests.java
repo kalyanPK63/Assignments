a82f592fb15c8abd4091c49d6e325fc9d43c1c41
kalyanPK63 <pavanchinna799@gmail.com>
2022-04-08 23:47:34 +0530

    Added Spring Security assignmnet

---
+++ Spring Security/Assignment 1/spring-boot_security/src/test/java/com/sq/springboot_security/SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sq.springboot_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
